1d3a1a1fb6a5ae55151392ac4a26b6e22af5b3e0
Quesada Jefferson (SO/OPM1.4-Hda) <quj5hda@bosch.com>
2020-08-24 13:05:33 -0600

    add test folder
    add test folder

---
+++ test/api.test.js
@@ -0,0 +1,47 @@
+process.env.NODE_ENV = "test";
+//supertest to execute api test
+const request = require("supertest");
+
+//get the app server
+const app = require("../server");
+
+//database connection
+const { connect, closeDatabase } = require("../database/mongoDatabase");
+
+beforeAll(async () => await connect());
+afterAll(async () => await closeDatabase());
+
+describe("Autos Api", () => {
+  it("Get Autos from API", async () => {
+    var autos = await request(app)
+      .get("/api/autos")
+      .expect("Content-Type", /json/)
+      .expect(200);
+    expect(autos.res.text).toBe("[]");
+  });
+
+  it("Post Autos correct csv File API", async () => {
+    await request(app)
+      .post("/api/autos")
+
+      .attach("csvFile", "./test/MOCK_DATA_SUCCESS.csv")
+      .field({ provider: "test" })
+      .expect(200);
+  });
+
+  it("Post Autos Incorrect csv File API", async () => {
+    await request(app)
+      .post("/api/autos")
+
+      .attach("csvFile", "./test/MOCK_DATA_ERROR.csv")
+      .field({ provider: "test" })
+      .expect(500);
+  });
+
+  it("Client page HTML PAGE", async () => {
+    await request(app)
+      .get("/")
+      .expect("Content-Type", "text/html; charset=UTF-8")
+      .expect(200);
+  });
+});
